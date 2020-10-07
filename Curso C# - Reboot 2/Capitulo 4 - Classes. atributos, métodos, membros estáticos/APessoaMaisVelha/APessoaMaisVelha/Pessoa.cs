40839f1c4dc4d817e1ab5a4f0b870abe375c20fe
Daniel Haag <danni.haag91@gmail.com>
2020-10-06 23:48:31 -0300

    teste

---
+++ Curso C# - Reboot 2/Capitulo 4 - Classes. atributos, métodos, membros estáticos/APessoaMaisVelha/APessoaMaisVelha/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APessoaMaisVelha
+{
+    class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public Pessoa LerDados()
+        {
+            Pessoa pessoa = new Pessoa();
+            Console.WriteLine("Nome:");
+            pessoa.Nome = Console.ReadLine();
+            Console.WriteLine("Idade:");
+            pessoa.Idade = int.Parse(Console.ReadLine());
+
+            return pessoa;
+        }
+    }
+}
---
+++ Curso C# - Reboot 2/Capitulo 4 - Classes. atributos, métodos, membros estáticos/APessoaMaisVelha/APessoaMaisVelha/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APessoaMaisVelha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa primeiraPessoa = new Pessoa();
+            Pessoa segundaPessoa = new Pessoa();
+
+            Console.WriteLine("Quem é a pessoa mais velha?");
+            Console.WriteLine("Dados da primeira pessoa:");
+            primeiraPessoa = primeiraPessoa.LerDados();
+            Console.WriteLine("Dados da segunda pessoa:");
+            segundaPessoa = segundaPessoa.LerDados();
+
+            if(primeiraPessoa.Idade > segundaPessoa.Idade)
+            {
+                Console.WriteLine(primeiraPessoa.Nome + " é mais velho(a) que " + segundaPessoa.Nome);
+            }
+            else if(primeiraPessoa.Idade == segundaPessoa.Idade)
+            {
+                Console.WriteLine("As duas pessoas tem a mesma idade.");
+            }
+            else
+            {
+                Console.WriteLine(segundaPessoa.Nome + " é mais velho(a) que " + primeiraPessoa.Nome);
+            }
+
+            Console.Read();
+        }
+    }
+}
